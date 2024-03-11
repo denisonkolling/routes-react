6f577c25745cef04a14fb1cd6373cfcd26f3f614
Denison Kolling <denisonkolling@gmail.com>
2024-03-11 17:09:28 -0300

    feat: add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import RoutesApp from './routes';
-import './App.css';
 import theme from '../src/styles/theme';
 import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../src/styles/globalStyles'
 
 function App() {
 	return (
 		<Router>
+			<GlobalStyles />
 			<ThemeProvider theme={theme}>
 			<RoutesApp />
 			</ThemeProvider>
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,29 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        outline: 0;
-        transition: 0.1s;
-    } 
-    .container {
-        margin: auto;
-        width: 50%;        
-        padding: 10px;
-        text-align: center;        
-    }
-    body {
-        background: ${(props) => props.theme.background};
-        color: ${props => props.theme.text};        
-    }
-    h1 {
-        color: ${props => props.theme.titles};       
-        font-family : ${props => props.theme.fonts.regular};
-    }
-    .accent {
-        background-color: ${props => props.theme.accent};
-        color: ${props => props.theme.text};
-        height: 30px;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-image: linear-gradient(-90deg, #7bc079 0%, #8ac788 100%);
+  font-family: Roboto, sans-serif;
+}
 `
