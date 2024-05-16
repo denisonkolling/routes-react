79dab5fb281dfca65cb425bebd14f847a319d8cd
Denison Kolling <denisonkolling@gmail.com>
2024-05-16 20:37:51 -0300

    feat: add trails db map

--- src/pages/Trails/index.jsx
+++ src/pages/Trails/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   Sidebar,
   Header,
@@ -12,8 +12,11 @@ import {
 } from "../../components";
 import { InputWrapper, StyledFontAwesomeIcon } from "./styles";
 import img from "../../../public/screenshots/trail.jpg";
+import { trails_db } from "../../../db/trails_db";
 
 const Trails = () => {
+  const [trails, setTrails] = useState(trails_db);
+
   return (
     <Wrapper>
       <Header />
@@ -32,54 +35,16 @@ const Trails = () => {
           </InputWrapper>
 
           <CardContainer>
-            <Card
-              image={img}
-              title={"Lake Tahoe"}
-              location={"Tahoe National Forest"}
-              rate={"⭐⭐⭐⭐"}
-              reviews={"(246)"}
-              distance={"Length 2.5mi - Time 2h 39m"}
-            ></Card>
-            <Card
-              image={img}
-              title={"Los Padres"}
-              location={"Los Padres National Forest"}
-              rate={"⭐⭐⭐⭐"}
-              reviews={"(312)"}
-              distance={"Length: 3.2 mi - Time: 2h 45min"}
-            ></Card>
-            <Card
-              image={img}
-              title={"Mount Rainier"}
-              location={"Mount Rainier National Park"}
-              rate={"⭐⭐⭐⭐"}
-              reviews={"(198)"}
-              distance={"Length: 2.8 mi - Time: 2h 36min"}
-            ></Card>
-            <Card
-              image={img}
-              title={"Rocky Mountain"}
-              location={"Rocky Mountain National Park"}
-              rate={"⭐⭐⭐⭐"}
-              reviews={"(189)"}
-              distance={"Length: 2.6 mi - Time: 2h 42min"}
-            ></Card>
-            <Card
-              image={img}
-              title={"Grand Canyon"}
-              location={"Grand Canyon National Park"}
-              rate={"⭐⭐⭐⭐"}
-              reviews={"(365)"}
-              distance={"Length: 2.4 mi - Time: 2h 38min"}
-            ></Card>
-            <Card
-              image={img}
-              title={"Olympic"}
-              location={"Olympic National Park"}
-              rate={"⭐⭐⭐⭐"}
-              reviews={"(277)"}
-              distance={"Length: 2.7 mi - Time: 2h 40min"}
-            ></Card>
+            {trails.map((trail) => (
+              <Card
+                image={trail.image}
+                title={trail.title}
+                location={trail.location}
+                rate={trail.rate}
+                reviews={trail.reviews}
+                distance={trail.distance}
+              />
+            ))}
           </CardContainer>
         </Content>
       </Container>
