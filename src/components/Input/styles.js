a0e10d5d5ce86ef5b9e85c6f6fa4cbc0d63cce81
Denison Kolling <denisonkolling@gmail.com>
2024-05-17 07:28:40 -0300

    style: update background theme color

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -8,6 +8,6 @@ export const InputStyled = styled.input`
 	font-size: 16px;
 	font-family: ${(props) => props.theme.fonts.regular};
 	font-weight: 400;
-	background-color: #f0f2f5;
+	background-color: ${(props) => props.theme.colors.secondary};//#f0f2f5;
 	border: none;
 `;
