cb114d8f33d61dff50a45b004df47817046cc068
Denison Kolling <denisonkolling@gmail.com>
2024-05-20 22:09:43 -0300

    style: center toast message position

--- src/services/notification.js
+++ src/services/notification.js
@@ -2,9 +2,9 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const successNotification = (message) => {
-    toast.success(message);
-}
+	toast.success(message, { position: 'bottom-center' });
+};
 
 export const errorNotification = (message) => {
-    toast.error(message);
-}
+	toast.error(message, { position: 'bottom-center' });
+};
